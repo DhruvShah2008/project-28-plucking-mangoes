c8b294cd10eb71b9bba1ec68feb5d807fe79c309
DhruvShah2008 <78470667+DhruvShah2008@users.noreply.github.com>
2021-05-25 12:07:46 +0530

    Add files via upload

--- mango.js
+++ mango.js
@@ -1,32 +1,30 @@
-class Mango{
-    constructor(x,y,r){
-     var options = {
-         restitution:0,
-         friction:0.5,
-         density:1,
-         isStatic:true
-     }
+class mango{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/mango.png")
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
 
-      this.x = x
-      this.y = y  
-      this.r = r
-      this.image = loadImage("images/mango.png")
-      this.body = Bodies.circle(this.x,this.y,this,r,options)
-      World.add(world,this.body)
-
-    }
-
-    display(){
-        var mangoA = this.body.position
-        push ();
-        translate (mangoA.x,mangoA.y);
-        rotate (this.body.angle)
-       fill (255,0,255)
-       imageMode (CENTER)
-       ellipseMode (CENTER)
-       this.image(this.image,0,0,this.r*2,this.r*2)
-       pop ();
-    }
-    
-    
+	display()
+	{
+		var mangoPos=this.body.position;	
+		push()
+		translate(mangoPos.x, mangoPos.y);
+		// rectMode(CENTER);
+		rotate(this.body.angle)
+		fill(255,0,255)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		pop()
+ }
 }
